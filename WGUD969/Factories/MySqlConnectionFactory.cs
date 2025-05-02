5b391656faefb8f0fe9570d9f84ebc009ff71350
Kyle Boehlen <kboehl3@wgu.edu>
2025-05-01 22:48:42 -0600

    mysql connection and docker container

---
+++ WGUD969/Factories/MySqlConnectionFactory.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace WGUD969.Factories
+{
+    public interface IMySqlConnectionFactory
+    {
+        MySqlConnection CreateConnection();
+    }
+
+    public class MySqlConnectionFactory : IMySqlConnectionFactory
+    {
+        private readonly IConfiguration _configuration;
+
+        public MySqlConnectionFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public MySqlConnection CreateConnection()
+        {
+            var connString = _configuration.GetConnectionString("SchedulingDatabase");
+            var connectionStrings = _configuration.GetSection("ConnectionStrings").GetChildren();
+            foreach (var connectionString in connectionStrings)
+            {
+                Debug.WriteLine($"Key: {connectionString.Key}, Value: {connectionString.Value}");
+            }
+
+            return new MySqlConnection(connString);
+        }
+    }
+}
--- WGUD969/Program.cs
+++ WGUD969/Program.cs
@@ -1,3 +1,9 @@
+using WGUD969.Factories;
+using WGUD969.Forms;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace WGUD969
 {
     internal static class Program
@@ -6,12 +12,40 @@ namespace WGUD969
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
+        //static void Main()
+        //{
+        //    // To customize application configuration such as set high DPI settings or default font,
+        //    // see https://aka.ms/applicationconfiguration.
+        //    ApplicationConfiguration.Initialize();
+        //    Application.Run(new Login());
+        //}
+
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            ServiceProvider = services.BuildServiceProvider();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
             ApplicationConfiguration.Initialize();
-            Application.Run(new Login());
+            Application.Run(ServiceProvider.GetRequiredService<Login>());
+        }
+
+        public static ServiceProvider ServiceProvider { get; private set; }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            services.AddSingleton<IConfiguration>(configuration);
+
+            // Register IConfiguration
+            services.AddSingleton(configuration);
+            services.AddSingleton<IMySqlConnectionFactory, MySqlConnectionFactory>();
+            services.AddTransient<Login>();
         }
     }
 }
